0865e906b619590f9c45eed6fb3e99551a54d623
John Mark Gabriel Caguicla <jmg.caguicla@guarandoo.me>
2025-04-20 21:34:18 +0800

    chore: flake module

--- flake.nix
+++ flake.nix
@@ -26,6 +26,59 @@
           subPackages = ["cmd/server"];
         };
       };
+
+      nixosModules.default = {
+        pkgs,
+        lib,
+        config,
+        ...
+      }: let
+        inherit (lib) mkIf;
+
+        cfg = config.services.neko;
+        settingsFormat = pkgs.formats.yaml {};
+        configFile = settingsFormat.generate "config.yaml" cfg.settings;
+      in {
+        options = {
+          services.neko = {
+            enable = lib.mkEnableOption "";
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = pkgs.neko;
+              description = "";
+            };
+            settings = lib.mkOption {
+              type = settingsFormat.type;
+              default = {
+                listenAddress = "127.0.0.1:8300";
+                handlers = [];
+              };
+              description = ''
+              '';
+            };
+          };
+        };
+        config = mkIf cfg.enable {
+          nixpkgs.overlays = [self.overlay];
+          systemd.services.neko = {
+            wantedBy = ["multi-user.target"];
+            after = ["network.target"];
+            description = "";
+
+            environment = {
+              NEKO_CONFIG = configFile;
+            };
+
+            serviceConfig = {
+              Restart = "always";
+              DynamicUser = "yes";
+              ExecStart = "${cfg.package}/bin/server";
+              AmbientCapabilities = ["CAP_NET_RAW"];
+              CapabilityBoundingSet = ["CAP_NET_RAW"];
+            };
+          };
+        };
+      };
     }
     // flake-utils.lib.eachDefaultSystem (
       system: let

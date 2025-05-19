a12df355eb9f1961131fea7b8c19f45847a341fa
John Mark Gabriel Caguicla <jmg.caguicla@guarandoo.me>
2025-05-19 02:44:32 +0000

    feat(probe/exec): capture stdout/stderr

--- pkg/probe/exec.go
+++ pkg/probe/exec.go
@@ -1,6 +1,7 @@
 package probe
 
 import (
+	"bytes"
 	"context"
 	"os/exec"
 	"sync"
@@ -25,12 +26,21 @@ func (p *execProbe) Probe(ctx context.Context, instance string, monitor string) 
 	tests := []core.Test{}
 	test := core.Test{Status: core.StatusUp, Target: p.name}
 
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
 	cmd := exec.CommandContext(ctx, p.name, p.args...)
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
 	err := cmd.Run()
 	if err != nil {
 		test.Status = core.StatusDown
 		test.Error = err
 	}
+
+	test.Extras["Stdout"] = stdout.String()
+	test.Extras["Stderr"] = stderr.String()
 
 	tests = append(tests, test)
 	return &core.Result{Tests: tests}, nil
